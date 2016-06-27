44123a709a2c17c80aa58e5eabed35ecc5b09927
Rails Test Flight <taken.reep@gmail.com>
2016-06-27 16:20:27 +0530

    Smoke test for creating todos

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160627101421) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
